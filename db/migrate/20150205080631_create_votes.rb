a9ed4254ff2757aac7559d131fb9312a190b9916
Carl Noresson <carlnoresson@gmail.com>
2015-02-11 11:40:16 +0700

    storing the reasons as a comma separated string

--- db/migrate/20150205080631_create_votes.rb
+++ db/migrate/20150205080631_create_votes.rb
@@ -2,10 +2,10 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.string  :vote_type
+      t.string  :reason_ids
 
       t.belongs_to :question
       t.belongs_to :user
-      t.belongs_to :reason
       t.timestamps
     end
   end
