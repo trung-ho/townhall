239e473a24ac913795f2ea394a6d3de8e0260a88
Carl Noresson <carlnoresson@gmail.com>
2015-03-20 12:25:49 +0700

    only use s3 for production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        
-      region:                ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.fog_credentials = {
+        provider:              'AWS',
+        aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        
+        region:                ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+    }
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
+  end
 end
