e394e4444da6c249bbdcbd1eb5b4ea2b2166d7d4
Carl Noresson <carlnoresson@gmail.com>
2015-03-20 12:17:53 +0700

    getting region from env vars

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
       provider:              'AWS',
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        
-      # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+      region:                ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = ENV['S3_BUCKET_NAME']
 end
