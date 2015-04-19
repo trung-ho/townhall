5b23148ac4b11470f6d5b89ce256b39e8c0d12f5
Carl Noresson <carlnoresson@gmail.com>
2015-04-19 15:13:44 +0700

    first tab should only display current polls

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,7 +2,7 @@ class OrganizationsController < ApplicationController
   before_action :set_organization, only: [:show, :edit, :update, :destroy]
 
   def show
-    @questions = @organization.questions  
+    @questions = @organization.questions.where("end_date > ?", DateTime.now)
     @pop_questions = @organization.questions.all[0..0]  
     @old_questions = @organization.questions.where("end_date < ?", DateTime.now)  
   end
