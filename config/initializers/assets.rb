5dd728ff145adecc5c0107f2f966abc80c49c818
Carl Noresson <carlnoresson@gmail.com>
2015-05-25 09:50:59 +0700

    not using the old themes assets anymore

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( active_admin.css organizer.css organizer.js error.css )
+Rails.application.config.assets.precompile += %w( error.css )
