e00e75e9de19dee2bec7c9294956e19ea4c4ba63
Carl Noresson <carlnoresson@gmail.com>
2015-05-27 10:24:59 +0700

    devise working again

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   layout :set_layout
-  before_filter :set_role
+  before_filter :set_role, only: [:new, :create]
 
   def set_layout
     'registrations' if RootSubdomain.matches?(request)
@@ -18,4 +18,12 @@ class Users::SessionsController < Devise::SessionsController
     def configure_sign_up_params
       devise_parameter_sanitizer.for(:sign_in) << [:remember_me]
     end
+
+  # def after_sign_in_path_for(resource)
+  #   if current_user.role == 'organizer'
+  #     #redirect_to organizer_dashboard_index_url(subdomain: current_user.main_organization)
+  #   else
+  #     super(resource)
+  #   end
+  # end
 end
